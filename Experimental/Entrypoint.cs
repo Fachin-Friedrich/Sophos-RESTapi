d17d02c10037f57bc35ddf7fdeb9a962e652083c
Marcel Rädeker <m.raedeker@fachin-friedrich.de>
2022-07-01 16:05:33 +0200

    Implemented API athuentification

--- Experimental/Entrypoint.cs
+++ Experimental/Entrypoint.cs
@@ -7,7 +7,10 @@ namespace Experimental
     {
         static void Main(string[] args)
         {
-            
+            var q = new SophosConnector(
+                id: "b1e43585-fa37-437f-9f26-1db5f3397efc",
+                secret: "e087863cb24ee3a43656acc82ea9a3c2a45315efd77e3e7058031cd56d3231df0da52525cd87cb855e0037515471dc504ea8"
+            );
         }
     }
 }
--- SophosRESTConnector/SophosConnector.cs
+++ SophosRESTConnector/SophosConnector.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using csJson;
 
 namespace SophosRESTConnector
 {
     public class SophosConnector
     {
         private HttpClient client;
+        private string clientid;
+        private string clientsecret;
 
-        public SophosConnector()
+        private jsonRoot verificationresult;
+
+        private string accesstoken
         {
+            get => verificationresult["access_token"].String;
+        }
 
+        private string refreshtoken
+        {
+            get => verificationresult["refresh_token"].String;
+        }
+
+        private string trackingid
+        {
+            get => verificationresult["trackingId"].String;
+        }
+
+        public SophosConnector( string id, string secret )
+        {
+            clientid = id;
+            clientsecret = secret;
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
+
+            var msg = new HttpRequestMessage();
+            msg.Method = HttpMethod.Post;
+            msg.RequestUri = new Uri("https://id.sophos.com/api/v2/oauth2/token");
+            msg.Content = new StringContent($"grant_type=client_credentials&client_id={id}&client_secret={secret}&scope=token" );
+            msg.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = client.SendAsync(msg).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            if( !response.IsSuccessStatusCode)
+            {
+                throw new Exception(
+                    $"Failed to establish connection to SOPHOS API with following context: {content}"
+                );
+            }
+
+            verificationresult = jsonRoot.Parse(content);
+            if( verificationresult["errorCode"].String != "success")
+            {
+                throw new Exception(
+                    $"Failed to authenticate against SOPHOS API with following error: {content}"
+                );
+            }
         }
     }
 }
