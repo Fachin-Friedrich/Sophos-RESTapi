9df7ff3e850ab0c27464e0bf03dd405cdccbd900
Marcel Rädeker <m.raedeker@fachin-friedrich.de>
2022-07-18 17:17:24 +0200

    Started implementing prototype

---
+++ Experimental/ConfigurationSet.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+
+namespace Experimental
+{
+    public struct InstigationPairing
+    {
+        public string MessageIsUp;
+        public string MessageIsDown;
+    }
+    
+    public struct ConfigurationSet
+    {
+
+        public string SophosId;
+        public string SophosSecret;
+
+        public int QueryTimeOffset;
+        public int TicketCreationThreshold;
+
+        public InstigationPairing[] SanctionedAlerts;
+
+        private const string QueryTimeStorage = "lastquery.bin";
+        private const int TimeTypeSize = 8;
+        public DateTime LastQueryTime
+        {
+            get
+            {
+                try
+                {
+                    var fhandle = System.IO.File.Open(
+                        QueryTimeStorage,
+                        System.IO.FileMode.Open
+                    );
+
+                    var buf = new byte[TimeTypeSize];
+                    if( fhandle.Read(buf, 0, TimeTypeSize) != TimeTypeSize)
+                    {
+                        fhandle.Close();
+                        throw new Exception();
+                    }
+                    fhandle.Close();
+
+                    long ticks = BitConverter.ToInt64(buf, 0);
+                    return new DateTime(ticks);
+
+                }catch
+                {
+                    var now = DateTime.Now;
+                    var today = now
+                        .AddHours(-now.Hour)
+                        .AddMinutes(-now.Minute)
+                        .AddSeconds(-now.Second)
+                        .AddMilliseconds(-now.Millisecond);
+                    return today;
+                }
+            }
+
+            set
+            {
+                var buf = BitConverter.GetBytes(value.Ticks);
+                var fhandle = System.IO.File.Open(
+                    QueryTimeStorage,
+                    System.IO.FileMode.OpenOrCreate
+                );
+
+                fhandle.Write(buf, 0, TimeTypeSize);
+                fhandle.Close();
+            }
+        }
+
+        public void DebugInit()
+        {
+            SophosId = "b1e43585-fa37-437f-9f26-1db5f3397efc";
+            SophosSecret = "e087863cb24ee3a43656acc82ea9a3c2a45315efd77e3e7058031cd56d3231df0da52525cd87cb855e0037515471dc504ea8";
+
+            QueryTimeOffset = 600;
+            TicketCreationThreshold = 300;
+
+            SanctionedAlerts = new InstigationPairing[] {
+                new InstigationPairing
+                {
+                    MessageIsUp = "Event::Firewall::FirewallREDTunnelUp",
+                    MessageIsDown = "Event::Firewall::FirewallREDTunnelDown"
+                },
+                new InstigationPairing
+                {
+                    MessageIsUp = "Event::Firewall::FirewallVPNTunnelUp",
+                    MessageIsDown = "Event::Firewall::FirewallVPNTunnelDown"
+                },
+                new InstigationPairing
+                {
+                    MessageIsUp = "Event::Firewall::FirewallGatewayUp",
+                    MessageIsDown = "Event::Firewall::FirewallGatewayDown"
+                },
+                new InstigationPairing
+                {
+                    MessageIsUp = "Event::Firewall::Reconnected",
+                    MessageIsDown = "Event::Firewall::LostConnectionToSophosCentral"
+                }
+            };
+
+        }
+
+        public ConfigurationSet( string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Experimental/Entrypoint.cs
+++ Experimental/Entrypoint.cs
@@ -1,34 +1,193 @@
 ﻿using System;
 using SophosRESTConnector;
 using SophosRESTConnector.Requests;
+using System.Collections.Generic;
 
 namespace Experimental
 {
+    internal class AlertRecord
+    {
+        public Alert alert;
+        public DateTime latest;
+        public Tenant tenant;
+
+        public AlertRecord( Alert palert, Tenant ptenant)
+        {
+            alert = palert;
+            latest = palert.IncidentTime;
+            tenant = ptenant;
+        }
+    }
+
     class Entrypoint
     {
-        static void Main(string[] args)
+        static ConfigurationSet config;
+        static SophosConnector sophos;
+        static Dictionary<string, AlertRecord> flaggedalerts;
+        
+        private static void writeLog( string msg)
         {
-            var sophos = new SophosConnector(
-                id: "b1e43585-fa37-437f-9f26-1db5f3397efc",
-                secret: "e087863cb24ee3a43656acc82ea9a3c2a45315efd77e3e7058031cd56d3231df0da52525cd87cb855e0037515471dc504ea8"
-            );
+            var col = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"[{DateTime.Now}] {msg}");
+            Console.ForegroundColor = col;
+        }
 
-            var param = new TimeParameter(14, 7, 2022, TimeConstraintType.After);
+        private static void ProcessInstigatorDown( Tenant tenant, Alert alert )
+        {
+            int sanctionindex = 0;
+            while (
+                sanctionindex < config.SanctionedAlerts.Length &&
+                alert.AlertType != config.SanctionedAlerts[sanctionindex].MessageIsDown
+            ) {
+                ++sanctionindex; 
+            }
 
-
-            foreach( var tenant in sophos.Tenants)
+            if ( sanctionindex == config.SanctionedAlerts.Length)
             {
-                var alerts = sophos.GetAlerts(tenant, param);
-                if( alerts.Length == 0)
-                {
-                    continue;
-                }
+                return;
+            }
 
-                foreach( var alert in alerts)
+            string recordkey = alert.ManagementAgentId + config.SanctionedAlerts[sanctionindex].MessageIsDown;
+            if ( flaggedalerts.ContainsKey(recordkey))
+            {
+                var record = flaggedalerts[recordkey];
+                if( alert.IncidentTime > record.latest)
                 {
-                    Console.WriteLine($"[{alert.IncidentTime}] {tenant.Name} - {alert.AlertType}");
+                    record.latest = alert.IncidentTime;
                 }
             }
+            else
+            {
+                flaggedalerts.Add(
+                    recordkey,
+                    new AlertRecord(alert,tenant)
+                );
+            }
+        }
+
+        private static object GetAssociatedTicket( Alert alert)
+        {
+            return null;
+        }
+
+        private static void ProcessInstigatorUp( Tenant tenant, Alert alert)
+        {
+            int sanctionindex = 0;
+            while (
+                sanctionindex < config.SanctionedAlerts.Length &&
+                alert.AlertType != config.SanctionedAlerts[sanctionindex].MessageIsUp
+            ) { ++sanctionindex; }
+
+            if (sanctionindex == config.SanctionedAlerts.Length)
+            {
+                return;
+            }
+
+            string recordkey = alert.ManagementAgentId + config.SanctionedAlerts[sanctionindex].MessageIsDown;
+            if ( flaggedalerts.ContainsKey(recordkey))
+            {
+                var record = flaggedalerts[recordkey];
+                if ( alert.IncidentTime > record.latest)
+                {
+                    writeLog($"Try Removing alert for {alert.ManagementAgentId}");
+                    flaggedalerts.Remove(recordkey);
+                }
+
+                return;
+            }
+
+            var associdatedticket = GetAssociatedTicket(alert);
+            if( associdatedticket != null)
+            {
+                //TODO Close ticket
+
+                return;
+            }
+
+            //TODO throw warning for unrecognized closed
+        }
+
+        private static void CreateAssociatedTicket( Tenant tenant, Alert alert)
+        {
+
+        }
+
+        private static void ForwardAlert( Tenant tenant, Alert alert )
+        {
+            int sanctionindex = 0;
+            while (
+                sanctionindex < config.SanctionedAlerts.Length && 
+                alert.AlertType != config.SanctionedAlerts[sanctionindex].MessageIsDown &&
+                alert.AlertType != config.SanctionedAlerts[sanctionindex].MessageIsUp
+            ) { ++sanctionindex; }
+
+            if (sanctionindex == config.SanctionedAlerts.Length)
+            {
+                return;
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            
+            flaggedalerts = new Dictionary<string, AlertRecord>();
+            config = new ConfigurationSet();
+            config.DebugInit();
+            sophos = new SophosConnector(
+                config.SophosId,
+                config.SophosSecret
+            );
+
+            while (true) //main loop
+            {
+                flaggedalerts.Clear();
+
+                var now = DateTime.Now;
+                var lastquery = config.LastQueryTime;
+                config.LastQueryTime = now;
+
+                var queryfilter = new TimeParameter(
+                    lastquery.AddSeconds(-config.QueryTimeOffset),
+                    TimeConstraintType.After
+                );
+
+                foreach( var tenant in sophos.Tenants)
+                {
+                    var alerts = sophos.GetAlerts(tenant, queryfilter);
+                    foreach( var alert in alerts)
+                    {
+                        ProcessInstigatorDown(tenant, alert );
+                    }
+
+                    foreach( var alert in alerts)
+                    {
+                        ProcessInstigatorUp(tenant, alert);
+                    }
+
+                    foreach( var alert in alerts)
+                    {
+                        ForwardAlert(tenant, alert);
+                    }
+                }
+
+                foreach( var record in flaggedalerts)
+                {
+                    writeLog($"Creating ticket for on {record.Value.tenant.Name} for {record.Value.alert.ManagementAgentId}");
+                    writeLog($"incident time {record.Value.alert.IncidentTime}");
+                    
+                    CreateAssociatedTicket(
+                        record.Value.tenant,
+                        record.Value.alert
+                    );
+                }
+
+                break;
+            }
+
         }
 
     } //End of class
